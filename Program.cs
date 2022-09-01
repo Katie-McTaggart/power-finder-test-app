7e6e07b77a8306681b89082205ca83eaf448a1d3
Katie McTaggart <katiemctaggartengineer@gmail.com>
2022-09-01 00:44:36 -0400

    Check for undesired values within while loop Replaced for loop which called checkNextPower function with a while loop.

--- Program.cs
+++ Program.cs
@@ -6,37 +6,17 @@ namespace PowerFinderTestApp
     class Program
     {
         static void Main(){
-            //Function finds a power and then determines if it contains 1, 2, 4, or 8.  
-            static bool checkNextPower(int baseValue, double power){
-                char[] forbiddenValues = new char[] {'1','2','4','8'};
-                double result;
+            double exponent = 1;
+            int baseValue = 16;
+            double power = Math.Pow(baseValue, exponent);
 
-                //Find the next power
-                result = Math.Pow(baseValue, power);
-
-                /*Check the result to see if it contains any one of the undesired values.
-                If it does we want to keep looking */
-                if(forbiddenValues.Any(result.ToString().Contains) == true){
-                    return false;
-                }
-                else{
-                    return true;
-                }
+            //Check powers until one is found without containing the undesired digits
+            while(new [] {"1", "2", "4", "8"}.Any(power.ToString().Contains)){
+                exponent++;
+                power = Math.Pow(baseValue, exponent);
             }
-
-            double j;
-            int num = 16;
-            bool potentialPower;
-            
-            //Loop through values until a potential power is found to not contain the undesired values
-            for(j = 1; j<100; j++){
-                potentialPower = checkNextPower(num, j);  
-
-                if(potentialPower == true){
-                    break;
-                }
-            }
-            Console.WriteLine("The result of 16^" + j + " does not contain 1,2, 4, or 8 in the value."); 
+                      
+            Console.WriteLine("The result of 16^" + exponent + " is " + power + " and does not contain 1,2, 4, or 8 in the value."); 
         }
     }
 }   
